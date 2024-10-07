738273094bbe0274f9287a805874ca9fca4dc5e7
Asus <Asus@LAPTOP-F814NMF7>
2024-10-08 00:40:11 +0400

    Add project files.

---
+++ cin homework/cin homework.cpp
@@ -0,0 +1,156 @@
+﻿
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    //1-----------------------------------------------------------------------------------
+
+
+
+    cout << "Bəstəkar-Qəmbər Hüseynli\nSair Tofiq Mütəllibova" << endl << endl;
+    cout << "Cib-cib cücələrim.'\nCib - cib - cib - cib cücələrim.\nMənim qəşəng cücələrim,\nTükü ipək cücələrim." << endl;
+
+
+    //2------------------------------------------------------------------------------------
+
+    //seir haqda bir sey tapa bilmedim...
+
+
+    //3------------------------------------------------------------------------------------
+
+
+    cout << "Masin alqi satqisi\n\nnovu - isdenilen nov\n\nisdenile nov masin alqi \nsatqisi bizde\n\ntel - +994 99 999 99 99 " << endl;
+
+
+    //4-------------------------------------------------------------------------------------
+
+    int R1 = 0;
+    cout << "R1 daxil edin - ";
+    cin >> R1;
+
+    int R2 = 0;
+    cout << "R2 daxil edin - ";
+    cin >> R2;
+
+    int R3 = 0;
+    cout << "R3 daxil edin - ";
+    cin >> R3;
+
+
+    double R0 = 1 / R1 + 1 / R2 + 1 / R3;
+    cout << "R0 beraberdir - " << R1 << endl;
+
+
+    //5---------------------------------------------------------------------------------------
+
+
+
+
+    int radius = 0;
+    cout << "radius daxil edin";
+    cin >> radius;
+
+    double pi = 3.14;
+
+    double S = pi * (radius * radius);
+    cout << "sahe - " << S << endl;
+
+    double L = 2 * pi * radius;
+    cout << "uzunluq - " << L << endl;
+
+
+
+
+    //6-----------------------------------------------------------------------------------------
+
+
+    int suret = 0;
+    int zaman = 0;
+    int tecil = 0;
+    cout << "suret - ";
+    cin >> suret;
+    cout << "zaman - ";
+    cin >> zaman;
+    cout << "tecil - ";
+    cin >> tecil;
+
+
+    double mesafe = suret * zaman + (tecil * (zaman * zaman) / 2);
+    cout << "mesafe - " << mesafe << endl;
+
+
+
+
+    //7------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+    int mesafe_2 = 0;
+    int zaman_2 = 0;
+    cout << "mesafe - ";
+    cin >> mesafe_2;
+    cout << "zaman - ";
+    cin >> zaman_2;
+
+
+    double suret_2 = mesafe / zaman;
+    cout << "suret - " << suret_2 << endl;
+
+
+
+
+
+
+    //8-------------------------------------------------------------------------------------------
+
+
+
+
+
+    //9--------------------------------------------------------------------------------------------
+
+    int mesafe_3 = 0;
+    cout << "mesafeni daxil edin - ";
+    cin >> mesafe_3;
+
+    int serfiyyat = 0;
+    cout << "100km re serfiyyati qeyd edin - ";
+    cin >> serfiyyat;
+
+    int dizel = 0;
+    cout << "dizelin 1 litr qiymetini daxil edin - ";
+    cin >> dizel;
+
+    int a92 = 0;
+    cout << "92 benzinin 1 litr qiymetini daxil edin - ";
+    cin >> a92;
+
+    int a95 = 0;
+    cout << "95 benzinin 1 litr qiymetini daxil edin - ";
+    cin >> a95;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
